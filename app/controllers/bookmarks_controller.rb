40df3956cc4da8f2543b15ca25e5411b78329a67
Tiago Collot <collot.tiago1@gmail.com>
2021-11-18 19:24:46 +0000

    Watch List

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,35 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie)
+  end
+
+  def find_bookmark
+    @bookmark = Bookmark.find(params[:bookmark_id])
+  end
+end
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,55 @@
+class ListsController < ApplicationController
+  # before_action :find_list, except: :destroy
+#   def index
+#     @lists = List.all
+#   end
+
+#   def show
+#     @list = List.find(params[:id])
+#   end
+
+#   def new
+#     @list = List.new
+#   end
+
+#   def create
+#     @list = List.new(list_params)
+#     @list.save
+#   end
+
+#    private
+
+#   def list_params
+#     params.require(:lists).permit(:name)
+#   end
+
+#   def find_list
+#     @list = list.find(params[:id])
+#   end
+# end
+  before_action :set_list, only: %i[show destroy]
+  def index
+    @lists = List.all
+  end
+  def new
+    @list = List.new
+  end
+  def show; end
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: 'List was successfully created.'
+    else
+      render :new
+    end
+  end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_list
+    @list = List.find(params[:id])
+  end
+  # Only allow a list of trusted parameters through.
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
